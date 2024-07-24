7f098a9af61c2779c0dc56b342cf3a8e23714088
Amin <moujahidamin@gmail.com>
2024-07-24 01:52:41 -0400

    Fixed the fields to show in the survey list

--- cs_survey/src/app/survey/survey.component.ts
+++ cs_survey/src/app/survey/survey.component.ts
@@ -13,7 +13,7 @@ import { Router } from '@angular/router';
   styleUrl: './survey.component.css'
 })
 export class SurveyComponent implements OnInit{
-
+  submitted = false;
   survey = new Survey(0, '', '', '', '', '', '', '', '', new Date(), [''], '', '', '');
   hear = [
     {value: 'Friends'}, 
@@ -23,7 +23,8 @@ export class SurveyComponent implements OnInit{
   ];
   likelihood = ['Very Likely', 'Likely', 'Unlikely'];
   //submitted = false;
-
+  likes:any ={
+  }
 // Uncomment when you link the API and Http stuff in the survey.service.ts
 constructor(private surveyService: SurveyService, private router: Router) {}
 
@@ -44,9 +45,20 @@ goToSurveyList(){
 }
 
 onSubmit() {
-  //TODO: Add input verification?
-  //this.submitted = true;
+  this.submitted = true;
   console.log(this.survey);
+
+  let likeKeys = Object.keys(this.likes)
+  let likes: string[] = [];
+  likeKeys.forEach((k) => {
+    if (this.likes[k]){
+      likes.push(k);
+    }
+  })
+  console.log(likes);
+
+  this.survey.likedMost = likes;
+
   this.savetheSurvey();
   //this.goToSurveyList()
     
