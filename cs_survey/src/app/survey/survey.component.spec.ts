74fb16263b0c3e21f385a5c25e52db00c993acb2
applepuddin <54207181+applepuddin@users.noreply.github.com>
2024-07-24 22:39:42 -0400

    Updated with comments survey.component.spec.ts

--- cs_survey/src/app/survey/survey.component.spec.ts
+++ cs_survey/src/app/survey/survey.component.spec.ts
@@ -1,3 +1,6 @@
+// Unit tests for the SurveyComponent. Verifies the component's creation and basic functionality.
+// Group: Amin Moujahid, Jack Bedinger, Allen Xavier Peter, Vishal Reddy Basani
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SurveyComponent } from './survey.component';
