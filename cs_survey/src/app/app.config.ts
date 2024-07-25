4d24327fbc5ae29c67caa3fe17041878491d9bc7
applepuddin <54207181+applepuddin@users.noreply.github.com>
2024-07-24 22:21:11 -0400

    Updated with comments app.config.ts

--- cs_survey/src/app/app.config.ts
+++ cs_survey/src/app/app.config.ts
@@ -1,3 +1,6 @@
+// Configures the main application settings and providers, including zone change detection, routing, HTTP client, and client hydration.
+// Group: Amin Moujahid, Jack Bedinger, Allen Xavier Peter, Vishal Reddy Basani
+
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
