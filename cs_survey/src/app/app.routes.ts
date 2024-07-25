d1553949fd9e4f8e3c14945253713666871545d9
applepuddin <54207181+applepuddin@users.noreply.github.com>
2024-07-24 22:03:36 -0400

    Updated with comments app.routes.ts

--- cs_survey/src/app/app.routes.ts
+++ cs_survey/src/app/app.routes.ts
@@ -1,3 +1,6 @@
+// Module to manage navigation and routing of the web application. Used to define routes to the “Home”, “Survey”, “Survey List” pages, allowing the user to access the pages.
+// Group: Amin Moujahid, Jack Bedinger, Allen Xavier Peter, Vishal Reddy Basani
+
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SurveyComponent } from './survey/survey.component';
